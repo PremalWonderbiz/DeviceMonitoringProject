1a5d2fe32dfaa88a852702a40a1fe0205b789ed9
PremalWonderbiz <premal.kadam@wonderbiz.in>
2025-11-17 14:52:07 +0530

    adding comment index.tsx to test pipeline v4

--- Frontend/device_monitoring_frontend/src/pages/index.tsx
+++ Frontend/device_monitoring_frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-// adding comment to test pipeline v3
+// adding comment to test pipeline v4
 import { Geist, Geist_Mono } from "next/font/google";
 import Badge from "@/components/customcomponents/Badge";
 import Sidebar from "@/components/customcomponents/SideBar";
