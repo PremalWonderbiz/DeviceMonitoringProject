c25a274e69c41d80e66d36a888e8598acb625284
PremalWonderbiz <premal.kadam@wonderbiz.in>
2025-12-11 16:16:53 +0530

    made changes in files to test all analysis runs

--- Backend/GatewayService/GatewayService/API/Program.cs
+++ Backend/GatewayService/GatewayService/API/Program.cs
@@ -1,7 +1,7 @@
 // adding comment to test pipeline v2
 //comment to test sonarqube ci check v2
 //tesing for sonarqube and coverity in pipeline v2
-//tesing for generic pipeline pipeline v23
+//tesing for generic pipeline pipeline v24
 using API.RealTime;
 
 var builder = WebApplication.CreateBuilder(args);
